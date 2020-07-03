6977649b6c8a2ab033bc3105d9259bb74017da34
iOsnaaente <bruno.bielsam.1205@hotmail.com>
2020-07-03 15:07:05 -0300

    update 0203072020

--- projeto/sonar.py
+++ projeto/sonar.py
@@ -1,82 +1,7 @@
+from SerialShark import *
 import pygame
 import random
 import math
-
-cor = {
-	"branco"   : [255,255,255],
-	"preto"    : [  0,  0,  0],
-	"cinza"    : [ 75, 75, 75],
-	"verde"    : [  0,200,  0],
-	"azul"     : [ 50, 50,200],
-	"vermelho" : [200,  0,  0]
-}
-
-screen_dimensions = [20*40,20*20]
-
-barraVermelha = pygame.Surface([18,5])
-barraVerde    = pygame.Surface([18,5])
-barraPreta    = pygame.Surface([20,7])
-
-barraVermelha.fill(cor["vermelho"])
-barraVerde.fill(cor["verde"])
-barraPreta.fill(cor["preto"])
-
-center = [screen_dimensions[0]/2, screen_dimensions[1]]
-
-pilhas =[]
-for i in range(360):
-	pilhas.append(0)
-
-piece_radial = []
-for i in range(180):
-	piece_radial.append(0)
-
-
-def plot_barras(width = 0, heigth=0):
-	for i in range(0,heigth,1):
-		barraPreta.blit(barraVerde, [1,1])
-		screen.blit(barraPreta, [width*20,(screen_dimensions[1])-(i*7)])
-	barraPreta.blit(barraVermelha, [1,1])	
-	screen.blit(barraPreta,[width*20,(screen_dimensions[1])-(heigth*7)])
-
-
-def catch_Dim():
-	for i in range(180):
-		pilhas[i] = int(100*math.sin(GtoR(i)))
-
-
-def GtoR(grau):
-	return grau*0.01745
-
-
-def drawPiece(raio, angulo=[0,0], cor_=0):
-
-	(x,y) = center
-	raioMax = 250
-	
-	angulo[0] = GtoR(angulo[0])
-	angulo[1] = GtoR(angulo[1])
-
-	xo,x1 = raio*math.cos(angulo[1]), raio*math.cos(angulo[0])
-	yo,y1 = raio*math.sin(angulo[1]), raio*math.sin(angulo[0])
-	xob,x1b = raioMax*math.cos(angulo[1]), raioMax*math.cos(angulo[0])
-	yob,y1b = raioMax*math.sin(angulo[1]), raioMax*math.sin(angulo[0])
-	
-	pygame.draw.polygon(screen, cor["vermelho"], [[x,y], [x+xob,y-yob],[x+x1b,y-y1b]],0)
-	pygame.draw.polygon(screen, cor["verde"]   , [[x,y], [x+xo,y-yo],[x+x1,y-y1]],4)
-	pygame.draw.line   (screen, cor["preto"]   , [x+xob,y-yob], [x+x1b,y-y1b],5) 
-	pygame.draw.line   (screen, cor["preto"]   , [x+xo,y-yo], [x+x1,y-y1],5) 
-
-	num = 5
-	for i in range(1,num):
-		pygame.draw.arc(screen, cor["preto"], [x-(i*50),y-(i*50), 2*(i*50),2*(i*50)], 0, GtoR(190), 1)
-		x1 = 250*math.cos(GtoR((180/num)*i))
-		y1 = 250*math.sin(GtoR((180/num)*i))
-		pygame.draw.line(screen, cor['preto'], [x,y], [x+x1,y-y1], 1)
-
-
-
-from SerialShark import *
 
 BAUDRATE = 9600
 
@@ -84,8 +9,21 @@ listaPortas = showSerialAvailable()
 surfaceListPorts = []
 
 flagComport = False
-
 comport = serial.Serial()
+
+cor = {
+	"branco"    : [255,255,255],
+	"preto"     : [  0,  0,  0],
+	"cinza"     : [ 75, 75, 75],
+	"verde"     : [  0,200,  0],
+	"azul"      : [ 50, 50,200],
+	"vermelho"  : [200,  0,  0],
+	'cinzaClaro':[200,200,200]
+}
+
+screen_dimensions = [20*40,20*20]
+center = [screen_dimensions[0]/2, screen_dimensions[1]]
+
 
 def detectPortSelect(coords = [0,0], flag = 0):
 	try:
@@ -104,8 +42,47 @@ def detectPortClose(coords = [0,0], flag = 0):
 	surface = [screen_dimensions[0]-110, screen_dimensions[1]-30]
 	if flagComport is True:
 		if coords[0] >= surface[0] and coords[0] <= surface[0]+100:
-			if coords[1] >= surface[1] and coords[1] <= surface[1]+100:
+			if coords[1] >= surface[1] and coords[1] <= surface[1]+30:
 				closeSerialConnection(comport)
+
+def attPortsAvailable():
+	surfaceListPorts = []
+	listaPortas = showSerialAvailable()
+
+
+piece_radial = []
+for i in range(180):
+	piece_radial.append(0)
+
+def GtoR(grau):
+	return grau*0.01745
+
+def drawPiece(raio, angulo=[0,0], _raioMax = 250, _num=5):
+
+	raioMax = _raioMax
+	(x,y) = center
+	num = _num
+
+	angulo[0] = GtoR(angulo[0])
+	angulo[1] = GtoR(angulo[1])
+
+	xo,x1 = raio*math.cos(angulo[1]), raio*math.cos(angulo[0])
+	yo,y1 = raio*math.sin(angulo[1]), raio*math.sin(angulo[0])
+	xob,x1b = raioMax*math.cos(angulo[1]), raioMax*math.cos(angulo[0])
+	yob,y1b = raioMax*math.sin(angulo[1]), raioMax*math.sin(angulo[0])
+	
+	pygame.draw.polygon(screen, cor["vermelho"], [[x,y], [x+xob,y-yob],[x+x1b,y-y1b]],0)
+	pygame.draw.polygon(screen, cor['verde']   , [[x,y], [x+xo,y-yo],[x+x1,y-y1]],4)	
+	pygame.draw.line   (screen, cor["preto"]   , [x+xob,y-yob], [x+x1b,y-y1b],5) 
+	pygame.draw.line   (screen, cor["preto"]   , [x+xo,y-yo], [x+x1,y-y1],5) 
+
+	prop = raioMax/num
+	for i in range(1,num):
+		pygame.draw.arc(screen, cor["preto"], [x-(i*prop),y-(i*prop), 2*(i*prop),2*(i*prop)], 0, GtoR(190), 1)
+		x1 = raioMax*math.cos(GtoR((180/num)*i))
+		y1 = raioMax*math.sin(GtoR((180/num)*i))
+		pygame.draw.line(screen, cor['preto'], [x,y], [x+x1,y-y1], 1)
+
 
 def drawRetangulo(fonte, dim = [0,0], texto="", cor = [0,0,0], enquadro=[5,5]):
 	text = fonte.render(texto,2,(0,0,0))
@@ -117,7 +94,7 @@ def drawRetangulo(fonte, dim = [0,0], texto="", cor = [0,0,0], enquadro=[5,5]):
 	border.blit(surface, (1,1))
 	return border
 
-def drawAll():
+def drawText():
 	textFonte  = pygame.font.SysFont(systemFont,30)
 	textFonte15  = pygame.font.SysFont(systemFont,15)
 
@@ -141,10 +118,25 @@ def drawAll():
 
 		if [0,(num+1)*33, port] not in surfaceListPorts:
 			surfaceListPorts.append([5, (num+1)*30 +10, port])
+	
 
-def attPortsAvailable():
-	surfaceListPorts = []
-	listaPortas = showSerialAvailable()
+where = []
+def drawProcessOptions(process):
+	options = ['DEMO', 'REMOTO', 'AUTOMÁTICO', 'MANUAL']
+	where = []
+	for i in range(len(options)):
+		txtOption = drawRetangulo(pygame.font.SysFont(systemFont,22), [110,30], options[i], cor['vermelho' if process is not i else 'verde' ])
+		screen.blit(txtOption, (screen_dimensions[0]-120, 5 + 40*i))
+		where.append([screen_dimensions[0]-120,  5+ 40*i])
+	return where
+
+def detectProcessOption(coord = [0,0], _where = [0,0]):
+	for surface in _where:
+		if coords[0] >= surface[0] and coords[0] <= surface[0]+110:
+			if coords[1] >= surface[1] and coords[1] <= surface[1]+30:
+				print(where.index(surface))
+				return where.index(surface)
+	return process
 
 pygame.init()
 
@@ -163,9 +155,10 @@ x,y = 0,0
 angulo = 0 
 raio = 0
 value = 0 
+process = 0
 
 while True:
-
+	screen.fill(cor['cinzaClaro'])
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
@@ -179,9 +172,11 @@ while True:
 			coords = pygame.mouse.get_pos()
 			detectPortClose(coords, flagComport)
 			comport = detectPortSelect(coords, flagComport)
-	
-	screen.fill(cor["branco"])
-	drawAll()
+			process = detectProcessOption(coords, where)
+
+
+	drawText()
+	where = drawProcessOptions(process)
 
 	if comport.is_open is True:
 		flagComport = True
@@ -193,19 +188,29 @@ while True:
 			print("ERRO NA CONVERSÃO DA SERIAL")
 	else:
 		flagComport = False
-		try:
-			angulo = angulo +1
-			if angulo == 180:
-				angulo = 0
-			value = value +1
-			piece_radial[angulo] = 100*math.cos(GtoR(value)) + 100*math.sin(GtoR(value)) 
-		except:
-			pass
+
 
 	for i in range(0,180,1):
-		drawPiece(piece_radial[i], [i,i+1], cor["verde"])		
-		if i == angulo :
-			drawPiece(piece_radial[i], [i ,i+1], cor["vermelho"])
+		drawPiece(piece_radial[i], [i,i+1])
 			
 	pygame.display.update()
 	clock.tick(60)
+	
+def processDEMO():
+	try:
+		angulo = angulo +1
+		if angulo == 180:
+			angulo = 0
+		value = value +1
+		piece_radial[angulo] = 100*math.cos(GtoR(value)) + 100*math.sin(GtoR(value)) 
+	except:
+		pass
+
+def processAUTO():
+	pass
+
+def processREMOTO():
+	pass
+
+def processMANUAL():
+	pass
